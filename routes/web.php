f8e036bb6392b3ddd381a962f02ff930bf21b483
Askhat <acxat_11@mail.ru>
2024-05-18 19:01:48 +0400

    feat: add /messages and /message routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/messages', [HomeController::class, 'messages'])->name('messages');
+Route::post('/message', [HomeController::class, 'message'])->name('message');
