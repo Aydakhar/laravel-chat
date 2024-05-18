68da348e84d99d87ea327813c74ec1d2ddac8280
Askhat <acxat_11@mail.ru>
2024-05-18 18:12:39 +0400

    feat: create Message model

---
+++ app/Models/Message.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    use HasFactory;
+    protected $fillable = ['sender_id', 'message'];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function getTimeAttribute(): string {
+        return date('d M Y H:i:s', strtotime($this->attributes['created_at']));
+    }
+}
