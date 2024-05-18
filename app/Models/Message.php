25919120fc8fdda92bbdd5f549f57cfb32969fb6
Askhat <acxat_11@mail.ru>
2024-05-18 19:01:06 +0400

    feat: add receiver_id column to messages table

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -9,10 +9,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Message extends Model
 {
     use HasFactory;
-    protected $fillable = ['sender_id', 'message'];
+    protected $fillable = ['sender_id', 'receiver_id', 'message'];
 
-    public function user(): BelongsTo {
+    public function sender(): BelongsTo {
         return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver(): BelongsTo {
+        return $this->belongsTo(User::class, 'receiver_id');
     }
 
     public function getTimeAttribute(): string {
