e75ba9eaa533b47d20f918b198cb6cc5ee585f38
Askhat <acxat_11@mail.ru>
2024-05-19 17:26:36 +0400

    fix: remove laravel ui

--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div id="app" data-user="{{ json_encode($user) }}"></div>
-</div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/messages', [HomeController::class, 'messages'])->name('messages');
 Route::post('/message', [HomeController::class, 'message'])->name('message');
